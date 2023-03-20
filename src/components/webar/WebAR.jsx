a6bfd103dc72c36b5f0ba65cc22480ad3aac1ba6
Wong Lok <wonglok@users.noreply.github.com>
2023-03-20 16:45:57 +0800

    ta

--- src/components/webar/WebAR.jsx
+++ src/components/webar/WebAR.jsx
@@ -223,7 +223,7 @@ export function WebAR() {
           <canvas ref={canvasRef} className='absolute top-0 left-0 w-full h-full'></canvas>
           <div ref={containerRef} className='absolute top-0 left-0 w-full h-full'>
             <Canvas shadows>
-              <group position={[0, 3, 4]} rotation={[Math.PI * -0.125, 0, 0]}>
+              <group position={[0, 3, 10]} rotation={[Math.PI * -0.125, 0, 0]}>
                 <PerspectiveCamera makeDefault far={500} near={0.1} ref={cameraRef}></PerspectiveCamera>
               </group>
 
