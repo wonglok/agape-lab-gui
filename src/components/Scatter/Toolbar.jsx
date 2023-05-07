e43d4ddcb01c93f501d46cf2ffcfd6bac5f4496c
Wong Lok <wonglok@users.noreply.github.com>
2023-05-07 08:00:15 +0800

    tool

--- src/components/Scatter/Toolbar.jsx
+++ src/components/Scatter/Toolbar.jsx
@@ -105,7 +105,12 @@ function R3f({ oid, object, refTracker }) {
       <group rotation={[0.3, 0, 0]}>{object && <primitive object={object}></primitive>}</group>
 
       <PerspectiveCamera ref={refCam} makeDefault position={[0, center[1], radius]}></PerspectiveCamera>
-      <OrbitControls makeDefault target={[0, center[1], 0]} enableZoom={false} enablePan={false}></OrbitControls>
+      <OrbitControls
+        makeDefault
+        target={[0, center[1], 0]}
+        enableZoom={metaKey}
+        enableRotate={false}
+        enablePan={false}></OrbitControls>
       <Environment preset='sunset'></Environment>
     </View>
   )
