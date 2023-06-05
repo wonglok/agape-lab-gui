0b3777cdba6f62ba492767046ca94955237f7d7a
Wong Lok <wonglok@users.noreply.github.com>
2023-06-06 07:36:05 +0800

    fafa

--- src/components/AR3Water/AR3Water.jsx
+++ src/components/AR3Water/AR3Water.jsx
@@ -91,6 +91,9 @@ function ARContent() {
   })
   return (
     <group>
+      <group position={[0, 0, 0]}>
+        <primitive object={camera}></primitive>
+      </group>
       <Environment files={`/envMap/evening_road_01_puresky_1k.hdr`}></Environment>
       <group position={[0, 0, -0.01]}>
         <ParticleRelay></ParticleRelay>
--- src/components/AR3Water/useAR.js
+++ src/components/AR3Water/useAR.js
@@ -171,7 +171,6 @@ export const useAR = create((set, get) => {
 
           if (pose) {
             get().updateCameraPose(pose, camera.quaternion, camera.position)
-            camera.position.z += 5
 
             ground.position.x = camera.position.x
             ground.position.z = camera.position.z
