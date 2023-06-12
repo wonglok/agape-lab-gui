5050916d65a316461059a7dc7a65d13bd171e64a
Wong Lok <wonglok@users.noreply.github.com>
2023-06-12 09:27:56 +0800

    save

--- src/components/MindAR/MindAR.jsx
+++ src/components/MindAR/MindAR.jsx
@@ -1,13 +1,19 @@
 import { useEffect, useRef, useState } from 'react'
 import {
+  AnimationMixer,
   Box3,
+  Clock,
   CylinderGeometry,
   EquirectangularReflectionMapping,
   FloatType,
+  GridHelper,
   MeshPhysicalMaterial,
   Vector3,
 } from 'three'
 import { BoxGeometry, Object3D } from 'three'
+import { DRACOLoader } from 'three/examples/jsm/loaders/DRACOLoader'
+import { FBXLoader } from 'three/examples/jsm/loaders/FBXLoader'
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader'
 import { RGBELoader } from 'three/examples/jsm/loaders/RGBELoader'
 import { Mesh, MeshBasicMaterial, MeshStandardMaterial, PlaneGeometry } from 'three147'
 import { create } from 'zustand'
@@ -26,7 +32,7 @@ export function MindAR() {
   let container = useRef()
 
   useEffect(() => {
-    import('mind-ar/dist/mindar-image-three.prod.js').then(({ MindARThree }) => {
+    import('mind-ar/dist/mindar-image-three.prod.js').then(async ({ MindARThree }) => {
       let mindarThree = new MindARThree({
         container: container.current,
         imageTargetSrc: `/2023/06/agape-ar-target/white/targets.mind`,
@@ -48,27 +54,54 @@ export function MindAR() {
       const geometry = new PlaneGeometry(1, 852 / 2896)
       const material = new MeshBasicMaterial({ color: 0x00ffff, transparent: true, opacity: 0.5 })
       const plane = new Mesh(geometry, material)
+      anchor.group.add(plane)
 
       let objectGrouper = new Object3D()
-      let volumeMesh = new Mesh(
-        new BoxGeometry(0.5, 0.5, 0.5),
-        new MeshStandardMaterial({ color: '#ffffff', roughness: 0, wireframe: true }),
-      )
 
-      let box3 = new Box3()
-      box3.expandByObject(volumeMesh)
-      let groupSizer = new Vector3()
-      box3.getSize(groupSizer)
-      objectGrouper.add(volumeMesh)
-      objectGrouper.position.set(0, 0, groupSizer.z / 2)
+      let tj = `/thankyou-jesus/tj3b.glb`
+      let glbLoader = new GLTFLoader()
+      let draco = new DRACOLoader()
+      draco.setDecoderPath(`/draco`)
+      glbLoader.setDRACOLoader(draco)
+      let fbxLoader = new FBXLoader()
+      let gltf = await glbLoader.loadAsync(tj)
+      let motions = {
+        mixer: new AnimationMixer(),
+        get: async (name, url) => {
+          let clip = (await fbxLoader.loadAsync(url)).animations[0]
+          motions[name] = motions.mixer.clipAction(clip, gltf.scene)
+        },
+      }
+      await motions.get('happy', `/thankyou-jesus/motion/Waving-Gesture.fbx`)
 
+      let setup = ({ gltf, motions }) => {
+        let autoScale = new Object3D()
+
+        autoScale.add(gltf.scene)
+
+        let box3 = new Box3()
+        box3.setFromObject(gltf.scene)
+        let autoSize = new Vector3()
+        box3.getSize(autoSize)
+
+        autoScale.scale.setScalar(0.5 / autoSize.length())
+
+        let grid = new GridHelper(1, 10, 0x00ffff, 0x00ffff)
+        objectGrouper.add(grid)
+        objectGrouper.add(autoScale)
+
+        motions.mixer.clipAction(gltf.animations[0], gltf.scene).play()
+      }
+
+      setup({ gltf, motions })
       anchor.group.add(objectGrouper)
-
-      anchor.group.add(plane)
 
       const start = async () => {
         await mindarThree.start()
+        let clock = new Clock()
         renderer.setAnimationLoop(() => {
+          let dt = clock.getDelta()
+          motions.mixer.update(dt)
           renderer.render(scene, camera)
         })
       }
