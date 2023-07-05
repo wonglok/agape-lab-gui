461cafcdfa319580baac57973d3a6a65fbcc3d60
Wong Lok <wonglok@users.noreply.github.com>
2023-07-05 17:01:28 +0800

    save

--- src/components/Stereo/Content.jsx
+++ src/components/Stereo/Content.jsx
@@ -1,4 +1,4 @@
-import { Box, MeshTransmissionMaterial, Sphere, Stars } from '@react-three/drei'
+import { Box, Environment, MeshTransmissionMaterial, Sphere, Stars } from '@react-three/drei'
 import { useFrame } from '@react-three/fiber'
 import { useRef } from 'react'
 
@@ -9,12 +9,16 @@ export function Content() {
   useFrame((st, dt) => {
     stars1.current.rotation.y += dt * 0.1
     sph.current.position.z = Math.sin(st.clock.elapsedTime) * 2
+
+    st.scene.backgroundIntensity = 0.2
   })
+
   return (
     <>
+      <Environment background files={`/envMap/evening_road_01_puresky_1k.hdr`}></Environment>
       <Stars ref={stars1}></Stars>
       <Sphere ref={sph} scale={2}>
-        <meshPhysicalMaterial roughness={0.01} transmission={1} thickness={3}></meshPhysicalMaterial>
+        <meshPhysicalMaterial roughness={0.01} metalness={0} transmission={1} thickness={1}></meshPhysicalMaterial>
       </Sphere>
     </>
   )
