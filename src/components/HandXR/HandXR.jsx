cb8d0d3c13b603a1c9660a27e9cc26342501ce33
Wong Lok <wonglok@users.noreply.github.com>
2023-07-21 13:09:58 +0800

    FA

--- src/components/HandXR/HandXR.jsx
+++ src/components/HandXR/HandXR.jsx
@@ -1,6 +1,6 @@
 import { createRoot } from 'react-dom/client'
 import React, { useState, useEffect, Fragment } from 'react'
-import { Hands, ARButton, XR } from '@react-three/xr'
+import { Hands, VRButton, XR } from '@react-three/xr'
 import { useThree, useFrame, Canvas } from '@react-three/fiber'
 import { Box, OrbitControls, Plane, Sphere, Sky, useMatcapTexture } from '@react-three/drei'
 import { usePlane, useBox, Physics, useSphere } from '@react-three/cannon'
@@ -91,7 +91,7 @@ function Scene() {
 
 export const HandXR = () => (
   <>
-    <ARButton />
+    <VRButton />
     <Canvas shadowMap>
       <XR>
         <Physics
