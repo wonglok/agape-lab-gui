53b756144782c33f9c957debef36609669f9a117
Wong Lok <wonglok@users.noreply.github.com>
2023-07-24 11:27:25 +0800

    fa

--- src/components/HandXR/HandXR.jsx
+++ src/components/HandXR/HandXR.jsx
@@ -1,17 +1,18 @@
-import React, { useState, useEffect, Fragment } from 'react'
+import React, { useState, useEffect, Fragment, useMemo } from 'react'
 import { Hands, VRButton, XR, XRButton } from '@react-three/xr'
 import { useThree, useFrame, Canvas } from '@react-three/fiber'
-import { Box, OrbitControls, Plane, Sphere, Sky } from '@react-three/drei'
+import { Box, OrbitControls, Plane, Sphere, Sky, useGLTF, useFBX, Environment } from '@react-three/drei'
 import { usePlane, useBox, Physics, useSphere } from '@react-three/cannon'
 import { joints } from './joints'
+import { AnimationMixer } from 'three'
 
-function Cube({ position, args = [0.06, 0.06, 0.06] }) {
+function Cube({ position, args = [0.065, 0.065, 0.065] }) {
   const [boxRef] = useBox(() => ({ position, mass: 1, args }))
   // const [tex] = useMatcapTexture('C7C0AC_2E181B_543B30_6B6270')
 
   return (
     <Box ref={boxRef} args={args} castShadow>
-      <meshStandardMaterial attach='material' color='#ffff00' />
+      <meshStandardMaterial attach='material' color='#ffff00' roughness={0} metalness={0.5} />
       {/* <meshMatcapMaterial attach='material' matcap={tex} /> */}
     </Box>
   )
@@ -48,7 +49,9 @@ function HandsReady(props) {
         setReady(true)
       }
     }, 500)
-    return () => clearInterval(id)
+    return () => {
+      clearInterval(id)
+    }
   }, [gl, ready])
 
   return ready ? props.children : null
@@ -69,10 +72,13 @@ function Scene() {
     position: [0, 0.8, 0],
     type: 'Static',
   }))
+
   return (
     <>
-      {/* <Sky /> */}
-      <Plane ref={floorRef} args={[5, 5]} receiveShadow>
+      <group scale={1} position={[0, -0.05, -2]}>
+        <Avatar></Avatar>
+      </group>
+      <Plane ref={floorRef} args={[10, 10]} receiveShadow>
         <meshStandardMaterial attach='material' color='#fff' transparent opacity={0.5} />
       </Plane>
       <Hands />
@@ -84,15 +90,49 @@ function Scene() {
       ))}
       <OrbitControls />
       <ambientLight intensity={0.5} />
-      <spotLight position={[1, 8, 1]} angle={0.3} penumbra={1} intensity={1} castShadow />
+      {/* <spotLight position={[1, 8, 1]} angle={0.3} penumbra={1} intensity={1} castShadow /> */}
     </>
   )
 }
 
+function Avatar() {
+  let glb = useGLTF(`/rpm/avatar/default-lok.glb`)
+  let motion = {
+    talk: useFBX(`/rpm/rpm-actions-emoji/talk-phone.fbx`),
+  }
+
+  let mixer = useMemo((r) => {
+    return new AnimationMixer()
+  }, [])
+  useFrame((st, dt) => {
+    mixer.update(dt)
+  })
+  useEffect(() => {
+    mixer.clipAction(motion.talk.animations[0], glb.scene).reset().play()
+  }, [glb.scene, mixer, motion])
+
+  return <primitive object={glb.scene}></primitive>
+}
+
 export const HandXR = () => (
   <>
+    <Canvas>
+      <XR>
+        <Physics
+          gravity={[0, -2, 0]}
+          iterations={20}
+          defaultContactMaterial={{
+            friction: 0.09,
+          }}>
+          <Scene />
+        </Physics>
+      </XR>
+
+      {/*  */}
+      <Environment files={`/lok/shanghai.hdr`}></Environment>
+    </Canvas>
     <div className='absolute bottom-0 left-0 flex items-center justify-center w-full'>
-      <div className='bg-lime-500'>
+      <div className='p-6 mb-5 bg-lime-500'>
         <XRButton
           /* The type of `XRSession` to create */
           mode={'AR'}
@@ -112,21 +152,9 @@ export const HandXR = () => (
           /** This callback gets fired if XR initialization fails. */
           onError={(error) => {}}>
           {/* Can accept regular DOM children and has an optional callback with the XR button status (unsupported, exited, entered) */}
-          {(status) => `${status === 'exited' ? 'Enter AR' : `Exit AR`}`}
+          {(status) => status}
         </XRButton>
       </div>
     </div>
-    <Canvas>
-      <XR>
-        <Physics
-          gravity={[0, -2, 0]}
-          iterations={20}
-          defaultContactMaterial={{
-            friction: 0.09,
-          }}>
-          <Scene />
-        </Physics>
-      </XR>
-    </Canvas>
   </>
 )
