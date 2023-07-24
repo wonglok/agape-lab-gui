4f23a1b4133017697048b2000ef87b31833f2a9b
Wong Lok <wonglok@users.noreply.github.com>
2023-07-24 23:29:43 +0800

    fafa

--- src/components/ClothSimAvatar/ClothSim.js
+++ src/components/ClothSimAvatar/ClothSim.js
@@ -72,7 +72,7 @@ export function ClothSim() {
         </group>
       </group>
 
-      {/* <gridHelper args={[50, 5, 0xff0000, 0xffff00]}></gridHelper> */}
+      {/* <gridHelper position={[0, 1, 0]} args={[50, 10, 0xff0000, 0xffff00]}></gridHelper> */}
       {/* <Box></Box> */}
       <group position={[0, -0.5, 0]}>
         <group rotation={[Math.PI * -0.5, 0, 0]}>
