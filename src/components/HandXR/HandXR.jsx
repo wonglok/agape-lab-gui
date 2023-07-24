6fbfb4c2d94e14ce93b248f3bb88d04d2bf4d79e
Wong Lok <wonglok@users.noreply.github.com>
2023-07-24 14:09:02 +0800

    fa

--- src/components/HandXR/HandXR.jsx
+++ src/components/HandXR/HandXR.jsx
@@ -271,7 +271,7 @@ export const HandXR = () => (
           /** This callback gets fired if XR initialization fails. */
           onError={(error) => {}}>
           {/* Can accept regular DOM children and has an optional callback with the XR button status (unsupported, exited, entered) */}
-          {(status) => status}
+          {(status) => (status === 'unsupported' ? `not supported` : `Enter`)}
         </XRButton>
       </div>
     </div>
