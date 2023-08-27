b0a24a16a8394d9478f41741eb7039ac4802b731
Wong Lok <wonglok@users.noreply.github.com>
2023-08-27 13:08:35 +0800

    fa

--- src/components/IKResolve/IKResolve.jsx
+++ src/components/IKResolve/IKResolve.jsx
@@ -11,10 +11,6 @@ import { useEffect, useRef } from 'react'
 // import { Object3D } from 'three147'
 
 let scene, camera, renderer, orbitControls, transformControls
-let mirrorSphereCamera
-
-const OOI = {}
-let IKSolver
 
 let stats, gui, conf
 const v0 = new THREE.Vector3()
@@ -23,7 +19,7 @@ export function IKResolve() {
   let domRef = useRef()
   useEffect(() => {
     domRef.current.innerHTML = ''
-    init({ container: domRef.current }).then(animate)
+    init({ container: domRef.current })
   }, [])
 
   return (
@@ -37,11 +33,9 @@ async function init({ container }) {
     followSphere: false,
     turnHead: false,
     ik_solver: true,
-    update: updateIK,
   }
 
   scene = new THREE.Scene()
-  scene.fog = new THREE.FogExp2(0xffffff, 0.17)
   scene.background = new THREE.Color(0xffffff)
 
   camera = new THREE.PerspectiveCamera(55, window.innerWidth / window.innerHeight, 0.001, 5000)
@@ -52,7 +46,7 @@ async function init({ container }) {
   const ambientLight = new THREE.AmbientLight(0xffffff, 1) // soft white light
   scene.add(ambientLight)
 
-  renderer = new THREE.WebGLRenderer({ antialias: true, logarithmicDepthBuffer: true })
+  renderer = new THREE.WebGLRenderer({ antialias: true, logarithmicDepthBuffer: false })
   renderer.setPixelRatio(window.devicePixelRatio)
   renderer.setSize(window.innerWidth, window.innerHeight)
   renderer.useLegacyLights = true
@@ -77,7 +71,6 @@ async function init({ container }) {
   let NAMES = {}
   let Clones = {}
   gltf.scene.traverse((n) => {
-    console.log(n.name)
     NAMES[n.name] = n
     Clones[n.name] = n.clone()
   })
@@ -93,92 +86,181 @@ async function init({ container }) {
   transformControls.addEventListener('mouseDown', () => (orbitControls.enabled = false))
   transformControls.addEventListener('mouseUp', () => (orbitControls.enabled = true))
 
-  let proxyMesh = new THREE.SkinnedMesh()
+  class CamHand {
+    constructor({ side = 'Left', scene, transformControls }) {
+      //
 
-  let targetBone = new THREE.Bone()
-  targetBone.name = 'target_bone'
-  transformControls.attach(targetBone)
+      let proxyMesh = new THREE.SkinnedMesh()
 
-  gltf.scene.getObjectByName('LeftHand').getWorldPosition(targetBone.position)
+      let targetBone = new THREE.Bone()
+      targetBone.name = 'target_bone'
+      // if (transformControls) {
+      //   transformControls.attach(targetBone)
+      // }
 
-  let bones = [
-    //
-    // NAMES.Hips,
-    // NAMES.Spine,
-    // NAMES.Spine1,
-    // NAMES.Spine2,
-    NAMES.LeftShoulder,
-    NAMES.LeftArm,
-    NAMES.LeftForeArm,
-    NAMES.LeftHand,
-    targetBone,
-  ]
+      this.targetBone = targetBone
+      gltf.scene.getObjectByName(`${side}Hand`).getWorldPosition(targetBone.position)
 
-  let skeleton = new THREE.Skeleton(bones)
+      let bones = [
+        //
+        // NAMES.Hips,
+        NAMES.Spine,
+        NAMES.Spine1,
+        NAMES.Spine2,
+        NAMES[`${side}Shoulder`],
+        NAMES[`${side}Arm`],
+        NAMES[`${side}ForeArm`],
+        NAMES[`${side}Hand`],
+        targetBone,
+      ]
 
-  proxyMesh.add(targetBone)
-  proxyMesh.bind(skeleton)
+      let skeleton = new THREE.Skeleton(bones)
 
-  const iks = [
-    {
-      target: bones.findIndex((r) => r.name === 'target_bone'), // ""
-      effector: bones.findIndex((r) => r.name === 'LeftHand'), // ""
-      links: [
+      proxyMesh.add(targetBone)
+      proxyMesh.bind(skeleton)
+
+      const iks = [
         {
-          index: bones.findIndex((r) => r.name === 'LeftForeArm'), // ""
-          // rotationMin: new THREE.Vector3(0, -1.8, 0),
-          // rotationMax: new THREE.Vector3(0, 1.8, 0),
-        },
-        {
-          index: bones.findIndex((r) => r.name === 'LeftArm'), // ""
-          // rotationMin: new THREE.Vector3(0, -1.8, 0),
-          // rotationMax: new THREE.Vector3(0, 1.8, 0),
-        },
-        {
-          index: bones.findIndex((r) => r.name === 'LeftShoulder'), // ""
-          rotationMin: Clones.LeftShoulder.rotation.clone(),
-          rotationMax: Clones.LeftShoulder.rotation.clone(),
-        },
-        // {
-        //   index: bones.findIndex((r) => r.name === 'Spine2'), // ""
-        //   rotationMin: Clones.Spine2.rotation.clone(),
-        //   rotationMax: Clones.Spine2.rotation.clone(),
-        // },
-        // {
-        //   index: bones.findIndex((r) => r.name === 'Spine1'), // ""
-        //   rotationMin: Clones.Spine1.rotation.clone(),
-        //   rotationMax: Clones.Spine1.rotation.clone(),
-        // },
-        //   {
-        //     index: 2, // "Spine1"
-        //     rotationMin: new THREE.Vector3(0, -1.8, 0),
-        //     rotationMax: new THREE.Vector3(0, 1.8, 0),
-        //   },
-        //   {
-        //     index: 1, // "Spine"
-        //     rotationMin: new THREE.Vector3(0, -1.8, 0),
-        //     rotationMax: new THREE.Vector3(0, 1.8, 0),
-        //   },
-        //   {
-        //     index: 0, // "Hips"
-        //     rotationMin: new THREE.Vector3(0, -1.8, 0),
-        //     rotationMax: new THREE.Vector3(0, 1.8, 0),
-        //     // rotationMin: new THREE.Vector3(1.2, -1.8, -0.4),
-        //     // rotationMax: new THREE.Vector3(1.7, -1.1, 0.3),
-        //   },
-      ],
-    },
-  ]
+          target: bones.findIndex((r) => r.name === 'target_bone'), // ""
+          effector: bones.findIndex((r) => r.name === `${side}Hand`), // ""
+          links: [
+            {
+              index: bones.findIndex((r) => r.name === `${side}ForeArm`), // ""
+              // rotationMin: new THREE.Vector3(0, -1.8, 0),
+              // rotationMax: new THREE.Vector3(0, 1.8, 0),
 
-  IKSolver = new CCDIKSolver(proxyMesh, iks)
-  const ccdikhelper = new CCDIKHelper(proxyMesh, iks, 0.02)
-  scene.add(ccdikhelper)
+              // rotationMin: {
+              //   x: Clones[`${side}Shoulder`].rotation.clone().x - 3.14 * 1.5,
+              //   y: Clones[`${side}Shoulder`].rotation.clone().y - 3.14 * 1.5,
+              //   z: Clones[`${side}Shoulder`].rotation.clone().z - 3.14 * 1.5,
+              // },
+              // rotationMax: {
+              //   x: Clones[`${side}Shoulder`].rotation.clone().x + 3.14 * 1.5,
+              //   y: Clones[`${side}Shoulder`].rotation.clone().y + 3.14 * 1.5,
+              //   z: Clones[`${side}Shoulder`].rotation.clone().z + 3.14 * 1.5,
+              // },
+            },
+            {
+              index: bones.findIndex((r) => r.name === `${side}Arm`), // ""
+              // rotationMin: new THREE.Vector3(0, -1.8, 0),
+              // rotationMax: new THREE.Vector3(0, 1.8, 0),
+
+              // rotationMin: {
+              //   x: Clones[`${side}Shoulder`].rotation.clone().x - 3.14 * 1,
+              //   y: Clones[`${side}Shoulder`].rotation.clone().y - 3.14 * 1,
+              //   z: Clones[`${side}Shoulder`].rotation.clone().z - 3.14 * 1,
+              // },
+              // rotationMax: {
+              //   x: Clones[`${side}Shoulder`].rotation.clone().x + 3.14 * 1,
+              //   y: Clones[`${side}Shoulder`].rotation.clone().y + 3.14 * 1,
+              //   z: Clones[`${side}Shoulder`].rotation.clone().z + 3.14 * 1,
+              // },
+            },
+            {
+              index: bones.findIndex((r) => r.name === `${side}Shoulder`), // ""
+              rotationMin: {
+                x: Clones[`${side}Shoulder`].rotation.clone().x - 0.1,
+                y: Clones[`${side}Shoulder`].rotation.clone().y - 0.1,
+                z: Clones[`${side}Shoulder`].rotation.clone().z - 0.1,
+              },
+              rotationMax: {
+                x: Clones[`${side}Shoulder`].rotation.clone().x + 0.1,
+                y: Clones[`${side}Shoulder`].rotation.clone().y + 0.1,
+                z: Clones[`${side}Shoulder`].rotation.clone().z + 0.1,
+              },
+            },
+            {
+              index: bones.findIndex((r) => r.name === 'Spine2'), // ""
+              rotationMin: {
+                x: Clones.Spine2.rotation.clone().x - 0.1,
+                y: Clones.Spine2.rotation.clone().y - 0.1,
+                z: Clones.Spine2.rotation.clone().z - 0.1,
+              },
+              rotationMax: {
+                x: Clones.Spine2.rotation.clone().x + 0.1,
+                y: Clones.Spine2.rotation.clone().y + 0.1,
+                z: Clones.Spine2.rotation.clone().z + 0.1,
+              },
+            },
+            {
+              index: bones.findIndex((r) => r.name === 'Spine1'), // ""
+              rotationMin: {
+                x: Clones.Spine1.rotation.clone().x - 0.2,
+                y: Clones.Spine1.rotation.clone().y - 0.2,
+                z: Clones.Spine1.rotation.clone().z - 0.2,
+              },
+              rotationMax: {
+                x: Clones.Spine1.rotation.clone().x + 0.2,
+                y: Clones.Spine1.rotation.clone().y + 0.2,
+                z: Clones.Spine1.rotation.clone().z + 0.2,
+              },
+            },
+            {
+              index: bones.findIndex((r) => r.name === 'Spine'), // ""
+              rotationMin: {
+                x: Clones.Spine.rotation.clone().x - 0.5,
+                y: Clones.Spine.rotation.clone().y - 0.5,
+                z: Clones.Spine.rotation.clone().z - 0.5,
+              },
+              rotationMax: {
+                x: Clones.Spine.rotation.clone().x + 0.5,
+                y: Clones.Spine.rotation.clone().y + 0.5,
+                z: Clones.Spine.rotation.clone().z + 0.5,
+              },
+            },
+
+            // {
+            //   index: bones.findIndex((r) => r.name === 'Hips'), // ""
+            //   rotationMin: {
+            //     x: Clones.Hips.rotation.clone().x - 0.0,
+            //     y: Clones.Hips.rotation.clone().y - 0.0,
+            //     z: Clones.Hips.rotation.clone().z - 0.0,
+            //   },
+            //   rotationMax: {
+            //     x: Clones.Hips.rotation.clone().x + 0.0,
+            //     y: Clones.Hips.rotation.clone().y + 0.0,
+            //     z: Clones.Hips.rotation.clone().z + 0.0,
+            //   },
+            // },
+
+            //   {
+            //     index: 2, // "Spine1"
+            //     rotationMin: new THREE.Vector3(0, -1.8, 0),
+            //     rotationMax: new THREE.Vector3(0, 1.8, 0),
+            //   },
+            //   {
+            //     index: 1, // "Spine"
+            //     rotationMin: new THREE.Vector3(0, -1.8, 0),
+            //     rotationMax: new THREE.Vector3(0, 1.8, 0),
+            //   },
+            //   {
+            //     index: 0, // "Hips"
+            //     rotationMin: new THREE.Vector3(0, -1.8, 0),
+            //     rotationMax: new THREE.Vector3(0, 1.8, 0),
+            //     // rotationMin: new THREE.Vector3(1.2, -1.8, -0.4),
+            //     // rotationMax: new THREE.Vector3(1.7, -1.1, 0.3),
+            //   },
+          ],
+        },
+      ]
+
+      let myIKSolver = new CCDIKSolver(proxyMesh, iks)
+      const ccdikhelper1 = new CCDIKHelper(proxyMesh, iks, 0.015)
+      scene.add(ccdikhelper1)
+
+      this.myIKSolver = myIKSolver
+      this.update = () => {
+        targetBone.updateMatrix()
+        targetBone.updateMatrixWorld()
+        myIKSolver.update()
+      }
+    }
+  }
 
   gui = new GUI()
   gui.add(conf, 'followSphere').name('follow sphere')
   gui.add(conf, 'turnHead').name('turn head')
   gui.add(conf, 'ik_solver').name('IK auto update')
-  gui.add(conf, 'update').name('IK manual update()')
   gui.open()
 
   window.addEventListener('resize', onWindowResize, false)
@@ -213,23 +295,29 @@ async function init({ container }) {
   })
   video.srcObject = stream
 
-  let initWP = new THREE.Vector3()
-  gltf.scene.getObjectByName('LeftHand').getWorldPosition(initWP)
-  let tp = new THREE.Vector3()
-  tp.copy(initWP)
-  let rAF = () => {
-    targetBone.position.lerp(tp, 0.1)
-    requestAnimationFrame(rAF)
-  }
-  requestAnimationFrame(rAF)
+  let initLeftWP = new THREE.Vector3()
+  let initRightWP = new THREE.Vector3()
+  gltf.scene.getObjectByName('LeftHand').getWorldPosition(initLeftWP)
+  gltf.scene.getObjectByName('RightHand').getWorldPosition(initRightWP)
+  let leftHandTP = new THREE.Vector3()
+  let rightHandTP = new THREE.Vector3()
+  leftHandTP.copy(initLeftWP)
+  rightHandTP.copy(initRightWP)
+
   let anim = async () => {
     let pose = await poseLandmarker.detect(video)
 
     if (pose.worldLandmarks[0]) {
-      tp.copy({
-        x: -pose.worldLandmarks[0][16].x + initWP.x,
-        y: -pose.worldLandmarks[0][16].y + initWP.y,
-        z: -pose.worldLandmarks[0][16].z + initWP.z,
+      leftHandTP.copy({
+        x: -pose.worldLandmarks[0][16].x * 1.0 + 0.0 * initRightWP.x,
+        y: -pose.worldLandmarks[0][16].y * 1.0 + initRightWP.y,
+        z: -pose.worldLandmarks[0][16].z * 1.0 + 0.0 * initRightWP.z,
+      })
+
+      rightHandTP.copy({
+        x: -pose.worldLandmarks[0][15].x * 1.0 + 0.0 * initLeftWP.x,
+        y: -pose.worldLandmarks[0][15].y * 1.0 + initLeftWP.y,
+        z: -pose.worldLandmarks[0][15].z * 1.0 + 0.0 * initLeftWP.z,
       })
     }
     video.requestVideoFrameCallback(anim)
@@ -237,35 +325,38 @@ async function init({ container }) {
   video.onloadeddata = async () => {
     video.requestVideoFrameCallback(anim)
   }
+
   video.autoplay = true
   video.style.position = 'absolute'
   video.style.left = '0px'
   video.style.top = '0px'
   video.style.width = '128px'
   video.playsInline = true
+  video.play()
   container.appendChild(video)
-}
 
-function animate() {
-  if (conf.ik_solver) {
-    updateIK()
+  let leftHand = new CamHand({ side: 'Left', scene })
+  let rightHand = new CamHand({ side: 'Right', scene })
+  let rAF = () => {
+    requestAnimationFrame(rAF)
+
+    scene.traverse(function (object) {
+      object.frustumCulled = false
+      if (object.isSkinnedMesh) object.geometry.computeBoundingSphere()
+    })
+
+    leftHand.targetBone.position.lerp(leftHandTP, 0.07)
+    leftHand.update()
+
+    rightHand.targetBone.position.lerp(rightHandTP, 0.07)
+    rightHand.update()
+
+    orbitControls.update()
+    renderer.render(scene, camera)
+
+    stats.update() // fps stats
   }
-
-  orbitControls.update()
-  renderer.render(scene, camera)
-
-  stats.update() // fps stats
-
-  requestAnimationFrame(animate)
-}
-
-function updateIK() {
-  if (IKSolver) IKSolver.update()
-
-  scene.traverse(function (object) {
-    object.frustumCulled = false
-    if (object.isSkinnedMesh) object.geometry.computeBoundingSphere()
-  })
+  requestAnimationFrame(rAF)
 }
 
 function onWindowResize() {
